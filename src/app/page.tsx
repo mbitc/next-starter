2e72bfe5e543487a133573faf221261812f27395
Marius Ratnikas <marius.ratnikas@gmail.com>
2024-05-22 13:14:26 +0300

    thirdweb-next-starter base

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,7 +7,7 @@ export default function Home() {
   return (
     <main className="p-4 pb-10 min-h-[100vh] flex items-center justify-center container max-w-screen-lg mx-auto">
       <div className="py-20">
-        <Header />
+        {/* <Header /> */}
 
         <div className="flex justify-center mb-20">
           <ConnectButton
@@ -19,7 +19,7 @@ export default function Home() {
           />
         </div>
 
-        <ThirdwebResources />
+        {/* <ThirdwebResources /> */}
       </div>
     </main>
   );
